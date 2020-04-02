12dc5826095a022ed1620ddf395a26edea199eb3
Allencl <Allen__cc@163.com>
2020-04-02 13:42:34 +0800

    1

---
+++ lib/index.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomePageqqq extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+
+      return Text("ki"); 
+    }
+
+}
